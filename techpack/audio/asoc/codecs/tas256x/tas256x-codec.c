IllusionX-Team/android_kernel

techpack/tas256x: Fix GCC warnings
In file included from ../include/linux/i2c.h:30,
                 from ../techpack/audio/asoc/codecs/tas256x/tas256x-codec.c:30:
../techpack/audio/asoc/codecs/tas256x/tas256x-codec.c: In function 'tas256x_info_profile':
../techpack/audio/asoc/codecs/tas256x/tas256x-codec.c:1245:32: error: format '%d' expects argument of type 'int', but argument 5 has type 'long int' [-Werror=format=]
 1245 |         dev_dbg(p_tas256x->dev,"%s: max profile = %d\n", __func__, uinfo->value.integer.max);
      |                                ^~~~~~~~~~~~~~~~~~~~~~~~            ~~~~~~~~~~~~~~~~~~~~~~~~
      |                                                                                        |
      |                                                                                        long int
../include/linux/device.h:1378:45: note: in definition of macro 'dev_dbg'
 1378 |                 dev_printk(KERN_DEBUG, dev, format, ##arg);     \
      |                                             ^~~~~~
../techpack/audio/asoc/codecs/tas256x/tas256x-codec.c:1245:52: note: format string is defined here
 1245 |         dev_dbg(p_tas256x->dev,"%s: max profile = %d\n", __func__, uinfo->value.integer.max);
      |                                                   ~^
      |                                                    |
      |                                                    int
      |                                                   %ld
../techpack/audio/asoc/codecs/tas256x/tas256x-codec.c: In function 'tas256x_fw_ready':
../techpack/audio/asoc/codecs/tas256x/tas256x-codec.c:1355:54: error: format '%d' expects argument of type 'int', but argument 3 has type 'size_t' {aka 'long unsigned int'} [-Werror=format=]
 1355 |         dev_err(pTAS256x->dev,"File size not match, %d %zu", pFW->size, fw_hdr->img_sz);
      |                                                     ~^       ~~~~~~~~~
      |                                                      |    

techpack/audio/asoc/codecs/tas256x/tas256x-codec.c
@@ -1242,7 +1242,7 @@ static int tas256x_info_profile(struct snd_kcontrol *kcontrol,
 #endif
 	uinfo->value.integer.min = -1;
 	uinfo->value.integer.max = max(-1, p_tas256x->ncfgs - 1);
-	dev_dbg(p_tas256x->dev,"%s: max profile = %d\n", __func__, uinfo->value.integer.max);
+	dev_dbg(p_tas256x->dev,"%s: max profile = %ld\n", __func__, uinfo->value.integer.max);
 	
 	return 0;
 }
@@ -1352,7 +1352,7 @@ static void tas256x_fw_ready(const struct firmware* pFW, void* pContext)
     fw_hdr->img_sz = SMS_HTONL(buf[offset], buf[offset + 1], buf[offset + 2], buf[offset + 3]);
     offset += 4;
     if (fw_hdr->img_sz != pFW->size) {
-        dev_err(pTAS256x->dev,"File size not match, %d %zu", pFW->size, fw_hdr->img_sz);
+        dev_err(pTAS256x->dev,"File size not match, %ld %u", pFW->size, fw_hdr->img_sz);
         goto EXIT;
     }
 

AOSP-Krypton/kernel_xiaomi_surya

Signed-off-by: taalojarvi <sreedevan05@gmail.com>

techpack/audio/asoc/codecs/tas256x/tas256x-codec.c
@@ -1242,7 +1242,7 @@ static int tas256x_info_profile(struct snd_kcontrol *kcontrol,
 #endif
 	uinfo->value.integer.min = -1;
 	uinfo->value.integer.max = max(-1, p_tas256x->ncfgs - 1);
-	dev_info(p_tas256x->dev,"%s: max profile = %d\n", __func__, uinfo->value.integer.max);
+	dev_info(p_tas256x->dev,"%s: max profile = %ld\n", __func__, uinfo->value.integer.max);
 	
 	return 0;
 }
@@ -1352,7 +1352,7 @@ static void tas256x_fw_ready(const struct firmware* pFW, void* pContext)
     fw_hdr->img_sz = SMS_HTONL(buf[offset], buf[offset + 1], buf[offset + 2], buf[offset + 3]);
     offset += 4;
     if (fw_hdr->img_sz != pFW->size) {
-        dev_err(pTAS256x->dev,"File size not match, %d %zu", pFW->size, fw_hdr->img_sz);
+        dev_err(pTAS256x->dev,"File size not match, %ld %u", pFW->size, fw_hdr->img_sz);
         goto EXIT;
     }
 